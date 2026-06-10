35740b5099cd95064f57b354911bf4ee57693ead
Varshini031 <varshininalla0@gmail.com>
2026-06-10 11:22:20 +0530

    added code for validate file

--- app/backend/routes/validate.py
+++ app/backend/routes/validate.py
@@ -1,7 +1,10 @@
 from fastapi import APIRouter
+from app.backend.models.schemas import IdeaRequest
+from app.backend.services.idea_analyzer import analyze_idea
 
 router = APIRouter()
 
+
 @router.post("/validate")
-def validate():
-    return {"message": "working"}
+def validate_idea(request: IdeaRequest):
+    return analyze_idea(request.idea)
